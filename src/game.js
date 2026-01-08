b38fe670e0b6a8cbeb147ded2ac8202085466ab6
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-01-08 10:34:24 +0100

    Add module for game logic

---
+++ src/game.js
@@ -0,0 +1,11 @@
+function placeTile(board, currentPlayer, index) {
+    index--;
+    if (index < 0 || index >= board.BOARD_TILES) {
+        return ;
+    }
+    board.board[index] = currentPlayer.mark;
+}
+
+export function playRound(board, players) {
+    placeTile(board, players.currentPlayer, 2);
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createBoard, displayBoard } from "./board.js";
 import { createPlayers } from "./players.js";
+import { playRound } from "./game.js";
 
 function main() {
     const board = createBoard();
     const players = createPlayers();
+    displayBoard(board);
+    playRound(board, players);
     displayBoard(board);
 }
 
